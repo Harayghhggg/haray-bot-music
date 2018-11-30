93dea03aba6d0babb5e80a2963b09cf77afa5709
Harayghhggg <44177526+Harayghhggg@users.noreply.github.com>
2018-11-30 05:53:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,43 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+      console.log('======================================')
+      console.log(`by haray`);
+      console.log('=======================================')
+});
+const developers = ["319624924769681412"]
+const adminprefix = "H";
+client.on('message', message => {
+        var argresult = message.content.split(` `).slice(1).join(' ');
+          if (!developers.includes(message.author.id)) return;
+          
+      if (message.content.startsWith(adminprefix + 'pl')) {
+              client.user.setGame(argresult);
+                message.channel.send(`**Status You   ${argresult}**`)
+      } else 
+            if (message.content.startsWith(adminprefix + 'wt')) {
+                  client.user.setActivity(argresult, {type:'WATCHING'});
+                      message.channel.send(`**Status You   ${argresult}**`)
+            } else 
+                  if (message.content.startsWith(adminprefix + 'ls')) {
+                        client.user.setActivity(argresult , {type:'LISTENING'});
+                            message.channel.send(`**Status You  ${argresult}**`)
+                  } else 
+                        if (message.content.startsWith(adminprefix + 'st')) {
+                                client.user.setGame(argresult, "https://www.twitch.tv/kokoseda");
+                                  message.channel.send(`**Status You ${argresult} **`)
+                        }
+      if (message.content.startsWith(adminprefix + 'setname')) {
+            client.user.setUsername(argresult).then
+                message.channel.send(`Changing The Name To ..**${argresult}** `)
+      } else
+          if (message.content.startsWith(adminprefix + 'setavatar')) {
+                client.user.setAvatar(argresult);
+                  message.channel.send(`Changing The Avatar To :**${argresult}** `);
+          }
 });
 
-
-
-
-
-
-
- 
-  client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`HARAY BOT | Hhelp`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
 
 
 
